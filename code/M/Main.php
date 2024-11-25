28ef8ba9ff70820c41a536b352cb846c54e1c976
MarcJ <144659851+jauniauMarc@users.noreply.github.com>
2024-11-25 17:05:21 +0000

    implémentation code

---
+++ code/M/Main.php
@@ -0,0 +1,7 @@
+<?php
+include 'Moyenne.php';
+
+$mes_notes=array(5,12,7,10);
+echo "La moyenne est de " .moyenne(notes: $mes_notes). "/20";
+
+?>
---
+++ code/tri_selection.php
@@ -0,0 +1,10 @@
+<?php
+
+$montableau = array("-1", "-5", "2", "4", "-6", "45", "22", "14");
+sort($montableau);
+foreach ($montableau as $key => $val) {
+	echo "Tab[" . $key . "] = " . $val . "\n</br>";
+	}
+	
+
+?>
