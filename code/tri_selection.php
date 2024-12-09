60987bb0067adf8d751b6488b108ed66d91600d5
MarcJ <144659851+jauniauMarc@users.noreply.github.com>
2024-12-09 15:48:15 +0000

    tri

--- code/tri_selection.php
+++ code/tri_selection.php
@@ -1,10 +1,29 @@
 <?php
 
+/*
 $montableau = array("-1", "-5", "2", "4", "-6", "45", "22", "14");
 sort($montableau);
 foreach ($montableau as $key => $val) {
 	echo "Tab[" . $key . "] = " . $val . "\n</br>";
 	}
-	
+*/	
 
+
+function tri_selection(&$t){
+    $n = count($t);
+    for($i=0;$i<$n-1;$i++){
+        $min=$i;
+        for($j=$i+1;$j<$n;$j++){
+            if($t[$j]<$t[$min]){
+                $min=$j;
+            }
+        }
+        if ($min !== $i){
+            $temp=$t[$i];
+            $t[$i]=$t[$min];
+            $t[$min]=$temp;
+        }
+    }
+    
+}
 ?>
